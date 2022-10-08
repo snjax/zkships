8227f507fc16dda6227bb6bbcb0f37630f22c721
Dmitry Vdovin <voidxnull@gmail.com>
2022-10-08 15:48:02 +0400

    First working version

--- hello_world_client/src/worker.ts
+++ hello_world_client/src/worker.ts
@@ -1,17 +1,20 @@
 import * as Comlink from 'comlink';
-import { setup_generic_prover_and_verifier } from '@noir-lang/barretenberg/dest/client_proofs';
+import { setup_generic_prover_and_verifier, create_proof, verify_proof } from '@noir-lang/barretenberg/dest/client_proofs';
+import { acir_from_bytes } from '@noir-lang/noir_wasm';
 
-const acirUrl = new URL('../../hello_world/build/main.acirw', import.meta.url);
-const witnessUrl = new URL('../../hello_world/build/main.tr', import.meta.url);
+const acirUrl = new URL('../../hello_world/build/main.acir', import.meta.url);
 
 async function generateProof(): Promise<Buffer> {
-  const acirData = await (await fetch(acirUrl)).json();
-  const witnessData = await (await fetch(witnessUrl)).arrayBuffer();
-  const [prover, verifier] = await setup_generic_prover_and_verifier(acirData);
+  const acir = acir_from_bytes(new Uint8Array(await (await fetch(acirUrl)).arrayBuffer()));
+  const witnessData = Buffer.alloc(128);
+  witnessData.fill(0);
+
+  const [prover, verifier] = await setup_generic_prover_and_verifier(acir);
   console.log('Generating proof...');
-  const proof = await prover.createProof(new Uint8Array(witnessData));
-  console.log('Done!');
-  await verifier.verifyProof(proof);
+  const proof = await create_proof(prover, acir, { a: 1, b: 2 });
+  console.log('Verifying proof...');
+  const result = await verify_proof(verifier, proof);
+  console.log('Verification result: ', result);
   return proof;
 }
 
