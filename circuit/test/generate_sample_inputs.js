6efce6fd60d37c8a0820c2bdfe820991f7476a4c
snjax <igor.gulamov@gmail.com>
2022-10-09 03:18:02 +0400

    add hash for state transition

--- circuit/test/generate_sample_inputs.js
+++ circuit/test/generate_sample_inputs.js
@@ -1,6 +1,8 @@
 const { BarretenbergWasm } = require('@noir-lang/barretenberg/dest/wasm');
 const { SinglePedersen } = require('@noir-lang/barretenberg/dest/crypto/pedersen');
 const { serialise_public_inputs } = require('@noir-lang/aztec_backend');
+const { buildMimc7 } = require('circomlibjs');
+const crypto = require("crypto");
 
 // import { compile, acir_from_bytes } from '@noir-lang/noir_wasm';
 // import { setup_generic_prover_and_verifier, create_proof, verify_proof } from '@noir-lang/barretenberg/dest/client_proofs';
@@ -28,6 +30,16 @@ function u3_list_to_bigint(l) {
 function bigint_to_bytes32(x) {
     let hex = x.toString(16);
     return "0x" + "0".repeat(64 - hex.length) + hex;
+}
+
+function ff_to_bytes32(x, F) {
+    let hex = F.toString(x, 16);
+    return "0x" + "0".repeat(64 - hex.length) + hex;
+}
+
+
+function randbigint(byteCount) {
+  return BigInt('0x' + crypto.randomBytes(byteCount).toString('hex'))
 }
 
 function flatten_object(o) {
@@ -61,6 +73,7 @@ let data = {
     ],
 };
 
+let salt = randbigint(10); //80 bit salt
 
 let raw_data = flatten_object(data);
 
@@ -68,15 +81,24 @@ let raw_data = flatten_object(data);
 
 (async() => { 
     const barretenberg = await BarretenbergWasm.new();
+    const mimc7 = await buildMimc7();
     await barretenberg.init();
 
 
     let pub_data = bigint_to_bytes32(u3_list_to_bigint(raw_data.slice(0, 16)));
+
+    let old_state_data = bigint_to_bytes32(u3_list_to_bigint(raw_data.slice(16, 16 + 9))*(1n<<80n)+salt);
+    let new_state_data = bigint_to_bytes32(u3_list_to_bigint(raw_data.slice(16 + 9, 16 + 9 + 9))*(1n<<80n)+salt);
+
+    let old_state_hash = mimc7.multiHash([mimc7.F.e(old_state_data)]);
+    let new_state_hash = mimc7.multiHash([mimc7.F.e(new_state_data)]);
     
 
-    console.log(`state_hash="${state_hash}"`);
+    console.log(`old_state_hash="${ff_to_bytes32(old_state_hash, mimc7.F)}"`);
+    console.log(`new_state_hash="${ff_to_bytes32(new_state_hash, mimc7.F)}"`);
+
     console.log(`pub_data="${pub_data}"`);
     console.log(`raw_data=${JSON.stringify(raw_data.map(e=>e.toString()))}`);
-
+    console.log(`salt="${bigint_to_bytes32(salt)}"`);
     
 })();
